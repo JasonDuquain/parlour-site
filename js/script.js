a0c1e2c669cd412cbcbbbac501348a03790699ff
jasonduquain <jduquain@gmail.com>
2018-10-06 23:24:05 -0400

    discuss and stats sects complete

--- js/script.js
+++ js/script.js
@@ -25,4 +25,47 @@ heroBtnsWrap.addEventListener('transitionend', (e) => {
 
 
 
+/******  Stats numbers increasing fast  *******/
+let statOne = document.querySelector('.stats__number--one');
+let numOne = 1; 
+let clearOne = setInterval(() => { 
+    numOne++;
+    statOne.textContent = numOne;
+    if (numOne === 35) {
+        clearInterval(clearOne);
+    } 
+}, 40);
+    
+let statTwo = document.querySelector('.stats__number--two');
+let numTwo = 1; 
+let clearTwo = setInterval(() => { 
+    numTwo++;
+    statTwo.textContent = numTwo;
+    if (numTwo === 12) {
+        clearInterval(clearTwo);
+    } 
+}, 120);
+    
+let statThree = document.querySelector('.stats__number--three');
+let numThree = 1; 
+let clearThree = setInterval(() => { 
+    numThree++;
+    statThree.textContent = numThree;
+    if (numThree === 100) {
+        clearInterval(clearThree);
+    } 
+}, 15);
+
+let statFour = document.querySelector('.stats__number--four');
+let numFour = 1; 
+let clearFour = setInterval(() => { 
+    numFour++;
+    statFour.textContent = numFour;
+    if (numFour === 140) {
+        clearInterval(clearFour);
+    } 
+}, 10);
+
+
+
 
